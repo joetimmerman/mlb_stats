e2f166417f43d7184a044417636df261ec49648f
Evan Marcey <evanmarcey@gmail.com>
2017-07-07 10:39:58 -0400

    Update adv_pitching_stats.sql

--- sql/adv_pitching_stats.sql
+++ sql/adv_pitching_stats.sql
@@ -4,8 +4,8 @@ SELECT	f1.playerID,
         ROUND((f1.walks+f1.hits)/(f1.outs/3),3) whip,
         ROUND((f1.hits-f1.home_runs)/(f1.outs+f1.hits+f1.sac-f1.strikeouts-f1.home_runs),3) babip,
         ROUND(((13*f1.fly_balls*(cf.home_runs/cf.fly_balls)) + 3*(f1.walks+f1.hit_by_pitch) - 2*f1.strikeouts)/(f1.outs/3) + cf.cFip,3) xFip,
-        f1.strikeouts/(f1.outs+f1.hits+f1.walks+f1.hit_by_pitch+f1.sac) strikeout_rate,
-        f1.walks/(f1.outs+f1.hits+f1.walks+f1.hit_by_pitch+f1.sac) walk_rate,
+        f1.strikeouts/(f1.out_atBats+f1.hits+f1.walks+f1.hit_by_pitch+f1.sac) strikeout_rate,
+        f1.walks/(f1.out_atBats+f1.hits+f1.walks+f1.hit_by_pitch+f1.sac) walk_rate,
         f1.home_runs/f1.fly_balls*100 hr_to_fly_ball_rate,
         f1.games_played,
         f1.outs+f1.hits at_bats,
@@ -17,6 +17,17 @@ SELECT	f1.playerID,
         f1.outs,
         f1.sac,
         f1.fly_balls,
+        f1.ground_balls,
+        f1.pop_ups,
+        ROUND(6.145 - 
+			16.986*(f1.strikeouts/(f1.hits+f1.out_atBats+f1.walks+f1.hit_by_pitch+f1.sac)) +
+            11.434*(f1.walks/(f1.hits+f1.out_atBats+f1.walks+f1.hit_by_pitch+f1.sac)) -
+            1.858*((f1.ground_balls-f1.fly_balls-f1.pop_ups)/(f1.hits+f1.out_atBats+f1.walks+f1.hit_by_pitch+f1.sac)) +
+            7.653*POW((f1.strikeouts/(f1.hits+f1.out_atBats+f1.walks+f1.hit_by_pitch+f1.sac)),2) -
+            ABS(6.664*POW(((f1.ground_balls-f1.fly_balls-f1.pop_ups)/(f1.hits+f1.out_atBats+f1.walks+f1.hit_by_pitch+f1.sac)),2)) +
+            10.130*(f1.strikeouts/(f1.hits+f1.out_atBats+f1.walks+f1.hit_by_pitch+f1.sac))*((f1.ground_balls-f1.fly_balls-f1.pop_ups)/(f1.hits+f1.out_atBats+f1.walks+f1.hit_by_pitch+f1.sac)) -
+            5.195*(f1.walks/(f1.hits+f1.out_atBats+f1.walks+f1.hit_by_pitch+f1.sac))*((f1.ground_balls-f1.fly_balls-f1.pop_ups)/(f1.hits+f1.out_atBats+f1.walks+f1.hit_by_pitch+f1.sac)),3)
+		SIERA,
         ROUND(cf.cFip,3) cFip
 FROM	(SELECT	pg.playerID,
 				substring(g.gameID,1,4) record_year,
@@ -48,13 +59,23 @@ FROM	(SELECT	pg.playerID,
 					END) fly_balls,
 				SUM(CASE
 						WHEN 	UPPER(ab.event) LIKE '%OUT%' 
-								OR UPPER(ab.event) LIKE '%DOUBLE PLAY%' 
-								OR UPPER(ab.event) LIKE '%TRIPLE PLAY%' 
-								OR UPPER(ab.event) LIKE '%DP%'
-                                OR UPPER(ab.event) LIKE '%SAC%'
+							OR UPPER(ab.event) LIKE '%SAC%'
 							THEN 1
+						WHEN	UPPER(ab.event) LIKE '%DOUBLE PLAY%' 
+							OR UPPER(ab.event) LIKE '%DP%'
+							THEN 2
+						WHEN	UPPER(ab.event) LIKE '%TRIPLE PLAY%' 
+							THEN 3
 						ELSE 0
 					END) outs,
+				SUM(CASE WHEN	UPPER(ab.event) LIKE '%OUT%'
+							OR	UPPER(ab.event) LIKE '%SAC%'
+                            OR	UPPER(ab.event) LIKE '%DOUBLE PLAY%'
+                            OR 	UPPER(ab.event) LIKE '%DP%'
+                            OR	UPPER(ab.event) LIKE '%TRIPLE PLAY%' 
+							THEN 1
+						ELSE 0
+					END) out_atbats,
 				SUM(CASE
 						WHEN ab.event IN ('Single','Double','Triple','Home Run')
 							THEN 1
@@ -64,7 +85,17 @@ FROM	(SELECT	pg.playerID,
 						WHEN upper(ab.event) LIKE '%SAC%'
 							THEN 1
 						ELSE 0
-					END) sac
+					END) sac,
+				SUM(CASE
+						WHEN upper(ab.event) LIKE '%GROUND%'
+							THEN 1
+						ELSE 0
+					END) ground_balls,
+				SUM(CASE
+						WHEN upper(ab.event) LIKE '%POP%'
+							THEN 1
+						ELSE 0
+					END) pop_ups
 		FROM	game g
 		JOIN	player_game pg
 			ON	pg.gameID = g.gameID 
