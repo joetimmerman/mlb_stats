632c31a3ff6b8105014ac7bd4757f55a60e06b95
Joe Timmerman <Joe.Timmerman@eccella.com>
2017-07-19 17:14:59 -0400

    Working on hit fx data

--- python/append_hit_fx.py
+++ python/append_hit_fx.py
@@ -1,33 +1,67 @@
 import json
 import mlbStats
 import urllib.request
+import time
+from multiprocessing.dummy import Pool as ThreadPool
 
 connection,conn = mlbStats.openConnection()
 
 #First get a list of all gamePKs
 
+start_time = time.time()
 bad_urls = 0
-good_urls = 0
+full_data_urls = 0
+no_data_urls = 0
 query = 'SELECT game_pk from game'
 conn.execute(query)
 print('Finished execution')
 game_pks = conn.fetchall()
 
 base_url = 'http://statsapi.mlb.com/api/v1/game/%s/feed/color'
-
+urls = []
 for game in game_pks:
-	curr_url = base_url % game['game_pk']
-	print(curr_url)
+	urls.append(base_url % game['game_pk'])
+
+def testURL(curr_url):
 	try:
 		with urllib.request.urlopen(curr_url) as url:
 			print(curr_url)
-			print(url.read().decode())
+			page = url.read().decode()
 			#data = json.loads(url.read())
-			good_urls += 1
+			if len(page) > 1000:
+				pass
+				#full_data_urls += 1
+			else:
+				pass
+				#no_data_urls += 1
+			return(page)
 	except Exception  as e:
-		bad_urls += 1
-		print(e)
+		pass
+		#bad_urls += 1
+		#print(e)
 
-print('good: '+ str(good_urls))
-print('bad: '+ str(bad_urls))
+pool = ThreadPool(4)
+results = pool.map(testURL, urls)
+pool.close()
+pool.join()
 
+print(len(results))
+
+for result in results:
+	try:
+		if result and len(result) > 1000:
+			full_data_urls += 1
+		else:
+			no_data_urls += 1
+	except Exception as e:
+		print(str(e))
+
+
+
+
+print('full: '+ str(full_data_urls))
+print('full: '+ str(no_data_urls))
+#print('bad: '+ str(bad_urls))
+
+elapsed_time = time.time() - start_time
+print(str(elapsed_time))
